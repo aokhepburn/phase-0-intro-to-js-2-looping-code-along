528a1855e53f991409d7a319f2da10115ab70910
Alice Hepburn <aliceokhepburn@gmail.com>
2023-05-30 20:21:53 -0400

    intro java loops passed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, eventName){
+    let thankyouCards = []
+    
+    for (let i = 0; i < name.length; i++){
+        thankyouCards.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+    }
+        
+    return thankyouCards
+}
+
+function countDown(n){
+    while(n >= 0){console.log(n--)}
+    return
+}
+
+
